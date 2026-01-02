e247a92cf420ed4ff4de399c3d0d3bce4cab51e9
developers <developers@ibstec.com>
2026-01-02 08:05:21 +0000

    update

--- routes/web.php
+++ routes/web.php
@@ -325,8 +325,8 @@ Route::group(['prefix' => '/', 'middleware' => 'auth'], function () {
 
     /** CRM Request */
     Route::post('/crm/request-reject', [CrmController::class, 'crmRequestReject'])->name('crmRequestReject');
-    Route::post('crmRequestRejectToNoResponse', [CrmController::class, 'crmRequestRejectToNoResponse'])->name('crmRequestRejectToNoResponse');
-    Route::post('crmRequestConfirmToNoResponse', [CrmController::class, 'crmRequestConfirmToNoResponse'])->name('crmRequestConfirmToNoResponse');
+    Route::post('crmRequestNoResponseToReject', [CrmController::class, 'crmRequestNoResponseToReject'])->name('crmRequestNoResponseToReject');
+    Route::post('crmRequestNoResponseToConfirm', [CrmController::class, 'crmRequestNoResponseToConfirm'])->name('crmRequestNoResponseToConfirm');
     Route::post('/crm/request-no-response', [CrmController::class, 'crmRequestNoResponse'])->name('crmRequestNoResponse');
     Route::post('crmRequestConfirm', [CrmController::class, 'crmRequestConfirm'])->name('crmRequestConfirm');
     Route::post('crmRequestSave', [CrmController::class, 'crmRequestSave'])->name('crmRequestSave');
