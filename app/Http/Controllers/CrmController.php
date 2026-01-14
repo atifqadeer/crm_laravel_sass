3ad183a099384f08649fa89777588056dfd4d887
developers <developers@ibstec.com>
2026-01-14 17:45:17 +0000

    update crm declined

--- app/Http/Controllers/CrmController.php
+++ app/Http/Controllers/CrmController.php
@@ -1265,12 +1265,10 @@ class CrmController extends Controller
                 // Subquery: latest CRM note per applicant-sale (for details)
                 $latestCrmNotes = DB::table('crm_notes as cn_latest')
                     ->select('cn_latest.applicant_id', 'cn_latest.sale_id', 'cn_latest.details as latest_details', 'cn_latest.created_at as latest_created_at')
-                    // ->where('cn_latest.status', 1)
                     ->whereIn("cn_latest.moved_tab_to", ["declined"])
                     ->whereIn('cn_latest.id', function ($q) {
                         $q->selectRaw('MAX(id)')
                             ->from('crm_notes')
-                            // ->where('status', 1)
                             ->whereIn("moved_tab_to", ["declined"])
                             ->groupBy('applicant_id', 'sale_id');
                     });
@@ -1282,6 +1280,7 @@ class CrmController extends Controller
                         ->whereIn('id', fn ($subQuery) => 
                             $subQuery->select(DB::raw('MAX(id)'))
                                 ->from('crm_notes')
+                                ->where('moved_tab_to', 'declined')
                                 ->groupBy('applicant_id', 'sale_id')
                         ),
                     'crm_notes',
