47f5c943e3d65d2f0aff46f95e4f9c22d01ee7e8
developers <developers@ibstec.com>
2026-01-14 17:00:57 +0000

    update to invoice

--- app/Http/Controllers/CrmController.php
+++ app/Http/Controllers/CrmController.php
@@ -1683,12 +1683,12 @@ class CrmController extends Controller
                 // Subquery: latest CRM note per applicant-sale (for details)
                 $latestCrmNotes = DB::table('crm_notes as cn_latest')
                     ->select('cn_latest.applicant_id', 'cn_latest.sale_id', 'cn_latest.details as latest_details', 'cn_latest.created_at as latest_created_at')
-                    // ->where('cn_latest.status', 1)
+                    ->where('cn_latest.status', 1)
                     ->whereIn("cn_latest.moved_tab_to", ["invoice", "final_save"])
                     ->whereIn('cn_latest.id', function ($q) {
                         $q->selectRaw('MAX(id)')
                             ->from('crm_notes')
-                            // ->where('status', 1)
+                            ->where('status', 1)
                             ->whereIn("moved_tab_to", ["invoice", "final_save"])
                             ->groupBy('applicant_id', 'sale_id');
                     });
